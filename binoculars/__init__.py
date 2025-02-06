f680ec2d608f7157f27e551941f264ecc6263966
coffebank <johndoe@example.com>
2025-02-06 16:47:20 +0300

    add deepseek class for test

--- binoculars/__init__.py
+++ binoculars/__init__.py
@@ -1,4 +1,5 @@
 from .detector import Binoculars
 from .llama_detector import LlamaDetector
+from .deepseek_detector import DeepSeek_Binoculars
 
-__all__ = ["Binoculars", "LlamaDetector"]
+__all__ = ["Binoculars", "LlamaDetector", "DeepSeek_Binoculars"]
