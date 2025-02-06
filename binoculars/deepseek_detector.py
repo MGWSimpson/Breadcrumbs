c8711c06165bc237210eac028a3caf9a6903f4c5
coffebank <johndoe@example.com>
2025-02-06 17:38:08 +0300

    test iden models

--- binoculars/deepseek_detector.py
+++ binoculars/deepseek_detector.py
@@ -25,7 +25,7 @@ DEVICE_2 = "cuda:1" if torch.cuda.device_count() > 1 else DEVICE_1
 
 class DeepSeek_Binoculars(object):
     def __init__(self,
-                 observer_name_or_path: str = "deepseek-ai/DeepSeek-R1-Distill-Qwen-7B",
+                 observer_name_or_path: str = "deepseek-ai/DeepSeek-R1-Distill-Llama-8B",
                  performer_name_or_path: str = "deepseek-ai/DeepSeek-R1-Distill-Llama-8B",
                  use_bfloat16: bool = True,
                  max_token_observed: int = 512,
