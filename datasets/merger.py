bbb0d2aa79fefd85e54a77a1ecbf49dae8c897ca
coffebank <johndoe@example.com>
2025-03-02 14:22:19 +0300

    Add new data for tests

--- datasets/merger.py
+++ datasets/merger.py
@@ -2,10 +2,7 @@ import json
 
 # Список путей к файлам, которые нужно объединить
 file_paths = [
-    r'E:\diplom\Trinoculars\datasets\summarization-dataset\rew_from_deepseek_r1_dp.json',
-    r'E:\diplom\Trinoculars\datasets\summarization-dataset\rew_from_gemini_chat_lp.json',
-    r'E:\diplom\Trinoculars\datasets\summarization-dataset\rew_from_o1-mini_two_part.json',
-    r'E:\diplom\Trinoculars\datasets\deep_seek_api\new_deep_ans.json'
+
 ]
 
 merged_dataset = []
@@ -20,7 +17,7 @@ for file_path in file_paths:
         max_id = max(item['id'] for item in dataset) + 1
         merged_dataset.extend(dataset)
 
-with open(r'E:\diplom\Trinoculars\datasets\ru_detection_dataset_two.json', 'w', encoding='utf-8') as file:
+with open(r'ex.json', 'w', encoding='utf-8') as file:
     json.dump(merged_dataset, file, ensure_ascii=False, indent=4)
 
-print("Merged dataset saved to 'ru_detection_dataset_two.json'.")
+print("Merged dataset saved to 'ex.json'.")
